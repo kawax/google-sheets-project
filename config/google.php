b98dcf2aeb48f31dbcc65326fb7609589311ef88
kawax <kawaxbiz@gmail.com>
2018-06-28 18:49:30 +0900

    scopes

--- config/google.php
+++ config/google.php
@@ -17,12 +17,13 @@ return [
     | https://developers.google.com/console
     |
     */
-    'client_id'       => env('GOOGLE_CLIENT_ID', ''),
-    'client_secret'   => env('GOOGLE_CLIENT_SECRET', ''),
-    'redirect_uri'    => env('GOOGLE_REDIRECT', ''),
-    'scopes'          => [\Google_Service_Sheets::DRIVE, \Google_Service_Sheets::SPREADSHEETS],
-    'access_type'     => 'offline',
-    'approval_prompt' => 'force',
+    'client_id'        => env('GOOGLE_CLIENT_ID', ''),
+    'client_secret'    => env('GOOGLE_CLIENT_SECRET', ''),
+    'redirect_uri'     => env('GOOGLE_REDIRECT', ''),
+    //    'scopes'          => [\Google_Service_Sheets::DRIVE, \Google_Service_Sheets::SPREADSHEETS],
+    'scopes'           => [\Google_Service_Sheets::DRIVE_READONLY, \Google_Service_Sheets::SPREADSHEETS_READONLY],
+    'access_type'      => 'offline',
+    'approval_prompt'  => 'force',
     'prompt'           => 'consent', //"none", "consent", "select_account" default:none
 
     /*
@@ -34,7 +35,7 @@ return [
     | a Server key, and not a Browser key.
     |
     */
-    'developer_key' => env('GOOGLE_DEVELOPER_KEY', ''),
+    'developer_key'    => env('GOOGLE_DEVELOPER_KEY', ''),
 
     /*
     |----------------------------------------------------------------------------
@@ -45,7 +46,7 @@ return [
     | app engine or compute engine will be used.
     |
     */
-    'service' => [
+    'service'          => [
         /*
         | Enable service account auth or not.
         */
@@ -54,7 +55,7 @@ return [
         /*
         | Path to service account json file
         */
-        'file' => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION', '')
+        'file'   => env('GOOGLE_SERVICE_ACCOUNT_JSON_LOCATION', ''),
     ],
 
     /*
@@ -69,5 +70,5 @@ return [
     | NOTE: If client id is specified here, it will get over written by the one above.
     |
     */
-    'config' => [],
+    'config'           => [],
 ];

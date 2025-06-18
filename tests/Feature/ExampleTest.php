7fe7a115abbd49564e161a965ef4e77d68d7eaf4
kawax <kawaxbiz@gmail.com>
2025-06-18 11:59:13 +0900

    Update ExampleTest.php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Revolution\Google\Sheets\Facades\Sheets;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -14,6 +15,22 @@ class ExampleTest extends TestCase
      */
     public function test_basic_test()
     {
+        Sheets::expects('spreadsheet')
+            ->andReturnSelf();
+        Sheets::expects('sheet')
+            ->andReturnSelf();
+        Sheets::expects('get')
+            ->andReturn(collect([
+                ['John Doe', 'Hello World', '2023-10-01'],
+                ['Jane Doe', 'Goodbye World', '2023-10-02'],
+            ]));
+
+        Sheets::expects('collection')
+            ->andReturn(collect([
+                ['name' => 'John Doe', 'message' => 'Hello World', 'created_at' => '2023-10-01'],
+                ['name' => 'Jane Doe', 'message' => 'Goodbye World', 'created_at' => '2023-10-02'],
+            ]));
+
         $response = $this->get('/');
 
         $response->assertStatus(200);

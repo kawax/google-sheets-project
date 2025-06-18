f55ffdb2cc5c8318bfb5247c699269f68eea1662
kawax <kawaxbiz@gmail.com>
2025-06-18 16:20:51 +0900

    Update form.blade.php

--- resources/views/livewire/sheets/form.blade.php
+++ resources/views/livewire/sheets/form.blade.php
@@ -1,22 +1,21 @@
 <div>
     <form wire:submit="post" class="space-y-6">
-        <flux:input 
-            wire:model="name" 
-            label="Name" 
-            placeholder="Enter your name" 
-            autofocus 
+        <flux:input
+            wire:model="name"
+            label="Name"
+            placeholder="Enter your name"
+            autofocus
         />
 
-        <flux:input 
-            wire:model="message" 
-            label="Message" 
-            placeholder="Enter your message" 
+        <flux:input
+            wire:model="message"
+            label="Message"
+            placeholder="Enter your message"
         />
 
-        <flux:button 
-            type="submit" 
+        <flux:button
+            type="submit"
             variant="primary"
-            wire:loading.attr="disabled"
             class="w-full"
         >
             Submit
